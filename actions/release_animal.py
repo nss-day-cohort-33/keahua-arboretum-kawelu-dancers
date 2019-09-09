e92bc86190f4ec1524147e799f4774d5ef2f2172
Matthew Caldwell <MCaldwell4242@gmail.com>
2019-09-09 14:24:52 -0500

    wip

--- actions/release_animal.py
+++ actions/release_animal.py
@@ -3,8 +3,14 @@ from animals import RiverDolphin
 def release_animal(arboretum):
     animal = None
 
-    print("1. River Dolphin")
-    print("2. Dragonfly")
+    print("1. Gold Dust Day Gecko")
+    print("2. River Dolphin")
+    print("3. Nene Goose")
+    print("4. Kikakapu")
+    print("5. Pueo")
+    print("6. Ulae")
+    print("7. Ope'ape'a")
+    print("8. Happy-Face Spider")
 
     choice = input("Choose animal to release > ")
 
