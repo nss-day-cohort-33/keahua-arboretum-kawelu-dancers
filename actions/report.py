4894a3dd34557f49a1ce7d8b3f75fafc2828af7a
Adam Knowles <acknowles524@gmail.com>
2019-09-11 14:07:00 -0500

    plants added to report

--- actions/report.py
+++ actions/report.py
@@ -3,30 +3,42 @@ def build_facility_report(arboretum):
         print(f'River [{str(river.id)[:8]}]')
         for animal in river.animals:
             print(f'\t {animal.species} [{str(animal.id)[:8]}]')
+        for plant in river.plants:
+            print(f'\t {plant.species} [{str(plant.id)[:8]}]')
 
     for grassland in arboretum.grasslands:
         print(f'Grassland [{str(grassland.id)[:8]}]')
         for animal in grassland.animals:
             print(f'\t {animal.species} [{str(animal.id)[:8]}]')
+        for plant in grassland.plants:
+            print(f'\t {plant.species} [{str(plant.id)[:8]}]')
 
     for swamp in arboretum.swamps:
         print(f'Swamp [{str(swamp.id)[:8]}]')
         for animal in swamp.animals:
             print(f'\t {animal.species} [{str(animal.id)[:8]}]')
+        for plant in swamp.plants:
+            print(f'\t {plant.species} [{str(plant.id)[:8]}]')
 
     for mountain in arboretum.mountains:
         print(f'Mountain [{str(mountain.id)[:8]}]')
         for animal in mountain.animals:
             print(f'\t {animal.species} [{str(animal.id)[:8]}]')
+        for plant in mountain.plants:
+            print(f'\t {plant.species} [{str(plant.id)[:8]}]')
 
     for coastline in arboretum.coastlines:
         print(f'Coastline [{str(coastline.id)[:8]}]')
         for animal in coastline.animals:
             print(f'\t {animal.species} [{str(animal.id)[:8]}]')
+        for plant in coastline.plants:
+            print(f'\t {plant.species} [{str(plant.id)[:8]}]')
 
     for forest in arboretum.forests:
         print(f'Forest [{str(forest.id)[:8]}]')
         for animal in forest.animals:
             print(f'\t {animal.species} [{str(animal.id)[:8]}]')
+        for plant in forest.plants:
+            print(f'\t {plant.species} [{str(plant.id)[:8]}]')
 
     input("\n\nPress any key to continue...")
